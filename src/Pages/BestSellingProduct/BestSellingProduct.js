4d9f26cd09da54a745f4703340d2f6ef6565a562
Dinesh-pp <dinesh@paripoorna.in>
2023-01-18 19:03:11 +0530

    data was changed to an Array Of Object

--- src/Pages/BestSellingProduct/BestSellingProduct.js
+++ src/Pages/BestSellingProduct/BestSellingProduct.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Style from "./Style.module.css";
 import jar from "../../assets/Images/jar.png";
 import pharma from "../../assets/Images/pharma.png";
@@ -12,10 +12,75 @@ import Vector14 from "../../assets/Images/Vector14.png";
 import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
 import sliderbtn from "../../assets/Images/sliderbtn.png";
 import sliderbtn1 from "../../assets/Images/sliderbtn1.png";
-import CurrencyRupeeIcon from '@mui/icons-material/CurrencyRupee';
-import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import CurrencyRupeeIcon from "@mui/icons-material/CurrencyRupee";
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
+import deva from "../../assets/Images/deva.png";
 
 const ProductList = () => {
+  const BestSellingProduct = [
+    {
+      Image: require("../../assets/Images/jar.png"),
+      Title: "Amber Pure Honey",
+      Unit: "1 UNIT",
+      Point: "4.5",
+      Amount: "18.00",
+      StarImage: require("../../assets/Images/Vector13.png"),
+      SubImage: require("../../assets/Images/Vector10.png"),
+      Count: "1",
+      AddImage: require("../../assets/Images/Vector11.png"),
+      AddToCart: "Add To Cart",
+    },
+    {
+      Image: require("../../assets/Images/avocado.png"),
+      Title: "Fresh Local Avocado",
+      Unit: "1 KG",
+      Point: "4.0",
+      Amount: "10.00",
+      StarImage: require("../../assets/Images/Vector13.png"),
+      SubImage: require("../../assets/Images/Vector10.png"),
+      Count: "4",
+      AddImage: require("../../assets/Images/Vector11.png"),
+      AddToCart: "Add To Cart",
+    },
+    {
+      Image: require("../../assets/Images/pharma.png"),
+      Title: "Pharma Cashew Butter",
+      Unit: "1 UNIT",
+      Point: "4.5",
+      Amount: "19.00",
+      StarImage: require("../../assets/Images/Vector13.png"),
+      SubImage: require("../../assets/Images/Vector10.png"),
+      Count: "2",
+      AddImage: require("../../assets/Images/Vector11.png"),
+      AddToCart: "Add To Cart",
+    },
+    {
+      Image: require("../../assets/Images/milk.png"),
+      Title: "Pure Dairy Milk",
+      Unit: "1 UNIT",
+      Point: "5.0",
+      Amount: "8.00",
+      StarImage: require("../../assets/Images/Vector13.png"),
+      SubImage: require("../../assets/Images/Vector10.png"),
+      Count: "2",
+      AddImage: require("../../assets/Images/Vector11.png"),
+      AddToCart: "Add To Cart",
+    },
+    {
+      Image: require("../../assets/Images/deva.png"),
+      Title: "Suzane Creamy Cupcake",
+      Unit: "1 UNIT",
+      Point: "3.5",
+      Amount: "14.00",
+      StarImage: require("../../assets/Images/Vector13.png"),
+      SubImage: require("../../assets/Images/Vector10.png"),
+      Count: "6",
+      AddImage: require("../../assets/Images/Vector11.png"),
+      AddToCart: "Add To Cart",
+    },
+  ];
+  const [bestProduct, setBestProduct] = useState(BestSellingProduct);
+  
   return (
     <div className="container mt-5 mb-5">
       <div class={Style.headContent}>
@@ -26,42 +91,52 @@ const ProductList = () => {
         </div>
       </div>
       <div className={Style.productDetails}>
-        <div className={`card ${Style.cardStyle}`}>
-          <div className={`card ${Style.cardImg}`}>
-            <div className={"d-flex justify-content-end p-2 "}>
-              <FavoriteBorderIcon className={Style.heartIcon} />
-            </div>
-            <div className="d-flex justify-content-center w-100">
-              <img className={Style.product} src={jar} />
-            </div>
-          </div>
-          <div className="d-flex flex-column">
-            <p className={Style.cardPrice}>Amber pure honey</p>
-            <div className="d-flex flex-row gap-2 align-items-center">
-              <p className={Style.unitText}>1 UNIT</p>
-              <div className={`d-flex flex-row gap-1 ${Style.starContent}`}>
-                <img className={Style.star} src={Vector13} />
-                <p className={`mb-0 ${Style.fixedPoint}`}>4.5</p>
+        {bestProduct.map((data, i) => {
+          return (
+            <div key={i} className={`card ${Style.cardStyle}`}>
+              <div className={`card ${Style.cardImg}`}>
+                <div className={"d-flex justify-content-end p-2 "}>
+                  <FavoriteBorderIcon className={Style.heartIcon} />
+                </div>
+                <div className="d-flex justify-content-center w-100">
+                  <img className={Style.product} src={data.Image} />
+                </div>
               </div>
-            </div>
+              <div className="d-flex flex-column">
+                <p className={Style.cardPrice}>{data.Title}</p>
+                <div className="d-flex flex-row gap-2 align-items-center">
+                  <p className={Style.unitText}>{data.Unit}</p>
+                  <div className={`d-flex flex-row gap-1 ${Style.starContent}`}>
+                    <img className={Style.star} src={data.StarImage} />
+                    <p className={`mb-0 ${Style.fixedPoint}`}>{data.Point}</p>
+                  </div>
+                </div>
 
-            <p className={Style.price}> <CurrencyRupeeIcon className={Style.rupee}/>18.00</p>
-            <div className="d-flex flex-row justify-content-between align-item-center mt-1 ">
-              <div className={`d-flex flex-row justify-content-between ${Style.addSub}`}>
-                <img className={Style.sub} src={Vector10} />
-                <p className="mb-0">1</p>
-                <img className={Style.add} src={Vector11} />
-              </div>
-              <div className={`d-flex align-items-center ${Style.mainCart}`}>
-                <p className={Style.addCart}>Add To Cart</p>
-                <ShoppingCartIcon className={Style.addCartIcon}/>
-                {/* <img className={Style.addCartIcon} src={Vector14} /> */}
+                <p className={Style.price}>
+                  <CurrencyRupeeIcon className={Style.rupee} />
+                  {data.Amount}
+                </p>
+                <div className="d-flex flex-row justify-content-between align-item-center mt-1 ">
+                  <div
+                    className={`d-flex flex-row justify-content-between ${Style.addSub}`}
+                  >
+                    <img className={Style.sub} src={data.SubImage} />
+                    <p className="mb-0">{data.Count}</p>
+                    <img className={Style.add} src={data.AddImage} />
+                  </div>
+                  <div
+                    className={`d-flex align-items-center ${Style.mainCart}`}
+                  >
+                    <p className={Style.addCart}>{data.AddToCart}</p>
+                    <ShoppingCartIcon className={Style.addCartIcon} />
+                  </div>
+                </div>
               </div>
             </div>
-          </div>
-        </div>
+          );
+        })}
 
-        <div className={`card ${Style.cardStyle}`}>
+        {/* <div className={`card ${Style.cardStyle}`}>
           <div className={`card ${Style.cardImg}`}>
             <div className={"d-flex justify-content-end p-2 "}>
               <FavoriteBorderIcon className={Style.heartIcon} />
@@ -88,13 +163,12 @@ const ProductList = () => {
               <div className="d-flex align-items-center">
                 <p className={Style.addCart}>Add To Cart</p>
                 <ShoppingCartIcon className={Style.addCartIcon}/>
-                {/* <img className={Style.addCartIcon} src={Vector14} /> */}
               </div>
             </div>
           </div>
-        </div>
+        </div> */}
 
-        <div className={`card ${Style.cardStyle}`}>
+        {/* <div className={`card ${Style.cardStyle}`}>
           <div className={`card ${Style.cardImg}`}>
             <div className={"d-flex justify-content-end p-2 "}>
               <FavoriteBorderIcon className={Style.heartIcon} />
@@ -121,13 +195,12 @@ const ProductList = () => {
               <div className="d-flex align-items-center">
                 <p className={Style.addCart}>Add To Cart</p>
                 <ShoppingCartIcon className={Style.addCartIcon}/>
-                {/* <img className={Style.addCartIcon} src={Vector14} /> */}
               </div>
             </div>
           </div>
-        </div>
+        </div> */}
 
-        <div className={`card ${Style.cardStyle}`}>
+        {/* <div className={`card ${Style.cardStyle}`}>
           <div className={`card ${Style.cardImg}`}>
             <div className={"d-flex justify-content-end p-2 "}>
               <FavoriteBorderIcon className={Style.heartIcon} />
@@ -154,13 +227,12 @@ const ProductList = () => {
               <div className="d-flex align-items-center">
                 <p className={Style.addCart}>Add To Cart</p>
                 <ShoppingCartIcon className={Style.addCartIcon}/>
-                {/* <img className={Style.addCartIcon} src={Vector14} /> */}
               </div>
             </div>
           </div>
-        </div>
+        </div> */}
 
-        <div className={`card ${Style.cardStyle}`}>
+        {/* <div className={`card ${Style.cardStyle}`}>
           <div className={`card ${Style.cardImg}`}>
             <div className={"d-flex justify-content-end p-2 "}>
               <FavoriteBorderIcon className={Style.heartIcon} />
@@ -189,11 +261,10 @@ const ProductList = () => {
               <div className="d-flex align-items-center">
                 <p className={Style.addCart}>Add To Cart</p>
                 <ShoppingCartIcon className={Style.addCartIcon}/>
-                {/* <img className={Style.addCartIcon} src={Vector14} /> */}
               </div>
             </div>
           </div>
-        </div>
+        </div> */}
       </div>
     </div>
   );
--- src/Pages/JustLandedItems/JustLandedItems.js
+++ src/Pages/JustLandedItems/JustLandedItems.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React,{useState} from "react";
 import Style from "./Style.module.css";
 import jar from "../../assets/Images/jar.png";
 import pharma from "../../assets/Images/pharma.png";
@@ -21,6 +21,70 @@ import CurrencyRupeeIcon from '@mui/icons-material/CurrencyRupee';
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 
 const JustLandedItems = () => {
+  const JustLandedItems = [
+    {
+      Image: require("../../assets/Images/chips.png"),
+      Title: "Jelly beans exxotics",
+      Unit: "1 UNIT",
+      Point: "4.5",
+      Amount: "18.00",
+      StarImage: require("../../assets/Images/Vector13.png"),
+      SubImage: require("../../assets/Images/Vector10.png"),
+      Count: "1",
+      AddImage: require("../../assets/Images/Vector11.png"),
+      AddToCart: "Add To Cart",
+    },
+    {
+      Image: require("../../assets/Images/juice.png"),
+      Title: "Growers cider",
+      Unit: "1 UNIT",
+      Point: "4.0",
+      Amount: "5.00",
+      StarImage: require("../../assets/Images/Vector13.png"),
+      SubImage: require("../../assets/Images/Vector10.png"),
+      Count: "2",
+      AddImage: require("../../assets/Images/Vector11.png"),
+      AddToCart: "Add To Cart",
+    },
+    {
+      Image: require("../../assets/Images/mockupgraphs.png"),
+      Title: "Fresh grapes",
+      Unit: "1 UNIT",
+      Point: "4.5",
+      Amount: "6.00",
+      StarImage: require("../../assets/Images/Vector13.png"),
+      SubImage: require("../../assets/Images/Vector10.png"),
+      Count: "4",
+      AddImage: require("../../assets/Images/Vector11.png"),
+      AddToCart: "Add To Cart",
+    },
+    {
+      Image: require("../../assets/Images/tomatoketchup.png"),
+      Title: "Heinz tomato ketchup",
+      Unit: "1 UNIT",
+      Point: "5.0",
+      Amount: "9.00",
+      StarImage: require("../../assets/Images/Vector13.png"),
+      SubImage: require("../../assets/Images/Vector10.png"),
+      Count: "2",
+      AddImage: require("../../assets/Images/Vector11.png"),
+      AddToCart: "Add To Cart",
+    },
+    {
+      Image: require("../../assets/Images/watermelonJuice.png"),
+      Title: "Sunstar Fresh Melon Juice",
+      Unit: "1 UNIT",
+      Point: "3.5",
+      Amount: "12.00",
+      StarImage: require("../../assets/Images/Vector13.png"),
+      SubImage: require("../../assets/Images/Vector10.png"),
+      Count: "3",
+      AddImage: require("../../assets/Images/Vector11.png"),
+      AddToCart: "Add To Cart",
+    },
+  ];
+  const [justLandedItems, setJustLandedItems] = useState(JustLandedItems);
+  
   return (
     <div className="container mt-5 mb-5">
       <div class={Style.headContent}>
@@ -31,41 +95,46 @@ const JustLandedItems = () => {
         </div>
       </div>
       <div className={Style.productDetails}>
-        <div className={`card ${Style.cardStyle}`}>
-          <div className={`card ${Style.cardImg}`}>
-            <div className={"d-flex justify-content-end p-2 "}>
-              <FavoriteBorderIcon className={Style.heartIcon} />
-            </div>
-            <div className="d-flex justify-content-center w-100">
-              <img className={Style.product} src={chips} />
-            </div>
-          </div>
-          <div className="d-flex flex-column">
-            <p className={Style.cardPrice}>Jelly beans exxotics</p>
-            <div className="d-flex flex-row gap-2 align-items-center">
-              <p className={Style.unitText}>1 UNIT</p>
-              <div className={`d-flex flex-row gap-1 ${Style.starContent}`}>
-                <img className={Style.star} src={Vector13} />
-                <p className={`mb-0 ${Style.fixedPoint}`}>4.5</p>
+        {justLandedItems.map((data,i)=>{
+          return(
+            <div key={i} className={`card ${Style.cardStyle}`}>
+            <div className={`card ${Style.cardImg}`}>
+              <div className={"d-flex justify-content-end p-2 "}>
+                <FavoriteBorderIcon className={Style.heartIcon} />
+              </div>
+              <div className="d-flex justify-content-center w-100">
+                <img className={Style.product} src={data.Image} />
               </div>
             </div>
-
-            <p className={Style.price}> <CurrencyRupeeIcon className={Style.rupee}/>18.00</p>
-            <div className="d-flex flex-row justify-content-between align-item-center mt-1 ">
-              <div className={`d-flex flex-row justify-content-between ${Style.addSub}`}>
-                <img className={Style.sub} src={Vector10} />
-                <p className="mb-0">1</p>
-                <img className={Style.add} src={Vector11} />
+            <div className="d-flex flex-column">
+              <p className={Style.cardPrice}>{data.Title}</p>
+              <div className="d-flex flex-row gap-2 align-items-center">
+                <p className={Style.unitText}>{data.Unit}</p>
+                <div className={`d-flex flex-row gap-1 ${Style.starContent}`}>
+                  <img className={Style.star} src={data.StarImage} />
+                  <p className={`mb-0 ${Style.fixedPoint}`}>{data.Point}</p>
+                </div>
               </div>
-              <div className="d-flex align-items-center">
-                <p className={Style.addCart}>Add To Cart</p>
-                <ShoppingCartIcon className={Style.addCartIcon}/>
-              </div> 
+  
+              <p className={Style.price}> <CurrencyRupeeIcon className={Style.rupee}/>{data.Amount}</p>
+              <div className="d-flex flex-row justify-content-between align-item-center mt-1 ">
+                <div className={`d-flex flex-row justify-content-between ${Style.addSub}`}>
+                  <img className={Style.sub} src={data.SubImage} />
+                  <p className="mb-0">{data.Count}</p>
+                  <img className={Style.add} src={data.AddImage} />
+                </div>
+                <div className="d-flex align-items-center">
+                  <p className={Style.addCart}>{data.AddToCart}</p>
+                  <ShoppingCartIcon className={Style.addCartIcon}/>
+                </div> 
+              </div>
             </div>
           </div>
-        </div>
-
-        <div className={`card ${Style.cardStyle}`}>
+  
+          )
+        })}
+       
+        {/* <div className={`card ${Style.cardStyle}`}>
           <div className={`card ${Style.cardImg}`}>
             <div className={"d-flex justify-content-end p-2 "}>
               <FavoriteBorderIcon className={Style.heartIcon} />
@@ -95,9 +164,9 @@ const JustLandedItems = () => {
               </div>
             </div>
           </div>
-        </div>
+        </div> */}
 
-        <div className={`card ${Style.cardStyle}`}>
+        {/* <div className={`card ${Style.cardStyle}`}>
           <div className={`card ${Style.cardImg}`}>
             <div className={"d-flex justify-content-end p-2 "}>
               <FavoriteBorderIcon className={Style.heartIcon} />
@@ -127,9 +196,9 @@ const JustLandedItems = () => {
               </div>
             </div>
           </div>
-        </div>
+        </div> */}
 
-        <div className={`card ${Style.cardStyle}`}>
+        {/* <div className={`card ${Style.cardStyle}`}>
           <div className={`card ${Style.cardImg}`}>
             <div className={"d-flex justify-content-end p-2 "}>
               <FavoriteBorderIcon className={Style.heartIcon} />
@@ -159,9 +228,9 @@ const JustLandedItems = () => {
               </div>
             </div>
           </div>
-        </div>
+        </div> */}
 
-        <div className={`card ${Style.cardStyle}`}>
+        {/* <div className={`card ${Style.cardStyle}`}>
           <div className={`card ${Style.cardImg}`}>
             <div className={"d-flex justify-content-end p-2 "}>
               <FavoriteBorderIcon className={Style.heartIcon} />
@@ -193,7 +262,7 @@ const JustLandedItems = () => {
               </div>
             </div>
           </div>
-        </div>
+        </div> */}
       </div>
     </div>
   );
