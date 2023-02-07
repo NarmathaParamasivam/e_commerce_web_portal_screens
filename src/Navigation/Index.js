4d65b56e98e354686fa82edb545918567e73b094
Dinesh-pp <dinesh@paripoorna.in>
2023-02-07 13:07:55 +0530

    changes

--- src/Navigation/Index.js
+++ src/Navigation/Index.js
@@ -1,10 +1,6 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Layout from "../Layout/Index";
-// import BannerImage from "../Pages/Banner/BannerImage.js";
-// import Category from "../Pages/Category/CategoryList.js";
-// import NewBrandList from "../Pages/NewlyArrivedBrands/NewBrandList.js";
-// import TrendingProduct from "../Pages/TrendingProducts/ProductList.js";
 import HomeScreen from "../HomeScreen/HomeScreen.js";
 
 function Navigation() {
--- src/Pages/MobileBanner/MobileBanner.js
+++ src/Pages/MobileBanner/MobileBanner.js
@@ -6,51 +6,46 @@ import GooglePay from "../../assets/Images/GooglePay.png";
 import Applelogo from "../../assets/Images/Applelogo.png";
 
 const MobileBanner = () => {
- 
   return (
     <div className={`container ${Styles.mainContainer}`}>
       <div className="row">
-
-      <img className={Styles.mobileBanner} src={phone} />
+        <img className={Styles.mobileBanner} src={phone} />
         <div className={`card ${Styles.cardDesign}`}>
           <div className={` ${Styles.cardText}`}>
             <h4 className={Styles.cardTitle}>Shop Faster With Foodmart App</h4>
             <p className={Styles.textContent}>
               Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sagittis
-              sed ptibus liberolectus nonet psryroin. Amet sed lorem posuere sit 
+              sed ptibus liberolectus nonet psryroin. Amet sed lorem posuere sit
               iaculis amet, ac urna. Adipiscing fames semper erat ac in
               suspendisse iaculis. Amet blandit tortor praesent ante vitae. A,
               enim pretiummi senectus magna. Sagittis sed ptibus liberolectus
               non et psryroin.
-             
             </p>
-            
+
             <div className={` ${Styles.gpayCard}`}>
               <div className={`card ${Styles.googlePay}`}>
-            <div className="d-flex gap-2">
-            
-              <img className={Styles.gpayLogo}src={GooglePay}/>
-              <div className="d-flex flex-column">
-            <span className={Styles.getGpay}> GET IT ON</span>
-            <span className={Styles.gPay}> Google Pay</span>
-            </div>
-            </div>
-            </div>
-<div className={`card ${Styles.googlePay}`}>
-            <div className="d-flex gap-2">
-            
-            <img className={Styles.gpayLogo}src={Applelogo}/>
-            <div className="d-flex flex-column">
-          <span className={Styles.getGpay}> Download on the</span>
-          <span className={Styles.gPay}> App Store</span>
-          </div>
-          </div>
-          </div>
+                <div className="d-flex gap-2">
+                  <img className={Styles.gpayLogo} src={GooglePay} />
+                  <div className="d-flex flex-column">
+                    <span className={Styles.getGpay}> GET IT ON</span>
+                    <span className={Styles.gPay}> Google Pay</span>
+                  </div>
+                </div>
+              </div>
+              <div className={`card ${Styles.googlePay}`}>
+                <div className="d-flex gap-2">
+                  <img className={Styles.gpayLogo} src={Applelogo} />
+                  <div className="d-flex flex-column">
+                    <span className={Styles.getGpay}> Download on the</span>
+                    <span className={Styles.gPay}> App Store</span>
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
         </div>
       </div>
-      </div>
+    </div>
   );
 };
 export default MobileBanner;
